3c88af339da88ab6a97708d00afa13a4fbd53183
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-11-26 22:40:05 +0800

    D15 to 16 lesson

---
+++ D15-16/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+// Create Express app
+const app = express();
+// Middleware
+app.use(express.json());
+
+
+// Connect to MongoDB
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+// Start the server
+app.listen(port, ()=>{
+    console.log(`Server is running in port ${port}`)
+    connectToDatabase();
+});
