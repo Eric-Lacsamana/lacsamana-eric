638370ac10893c77434fb9775fcb07e60859bf9f
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-11-30 13:55:51 +0800

    products api integration

--- D18/activity/src/pages/Products.jsx
+++ D18/activity/src/pages/Products.jsx
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col, Container } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
 import ProductDetailsModal from '../components/ProductDetailsModal';
-import { useEffect } from 'react';
 
 function ProductPage() {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [products, setProducts] = useState([]);
   const [error, setError] = useState(null);
-  useEffect(()=>{
-    const fetchProducts = async () =>{
+
+  useEffect(() => {
+    const fetchProducts = async () => {
       const response = await fetch('https://fakestoreapi.com/products');
       if (!response.ok) {
         setError('Error fetching products');
       }
-
-      const data =  await response.json();
-      console.log('data', data);
+      const data = await response.json();
       setProducts(data);
-    }
+    };
 
     fetchProducts();
-  }, [])
+  }, []);
 
   const handleViewDetails = (product) => {
     setSelectedProduct(product);
@@ -39,7 +37,7 @@ function ProductPage() {
     <Container className="mt-5">
       <Row>
         {products.map((product, index) => (
-          <Col key={index} md={4} className="mb-4">
+          <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
             <ProductCard product={product} onViewDetails={handleViewDetails} />
           </Col>
         ))}
