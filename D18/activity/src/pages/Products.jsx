55473d3971633a2d0edbc30b132c75a19cd90595
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-11-30 12:45:05 +0800

    initial fetching products

--- D18/activity/src/pages/Products.jsx
+++ D18/activity/src/pages/Products.jsx
@@ -3,28 +3,45 @@ import PropTypes from 'prop-types';
 import { Row, Col, Container } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
 import ProductDetailsModal from '../components/ProductDetailsModal';
+import { useEffect } from 'react';
 
 function ProductPage() {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(()=>{
+    const fetchProducts = async () =>{
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        setError('Error fetching products');
+      }
 
-  // Todo: replace with api call fetch 
-  const products = [
-    {
-      title: "Example Product 1",
-      price: 29.99,
-      description: "This is a detailed description of the product.",
-      category: "Electronics",
-      image: "https://via.placeholder.com/400",
-    },
-    {
-      title: "Example Product 2",
-      price: 49.99,
-      description: "Another detailed description of a different product.",
-      category: "Home Appliances",
-      image: "https://via.placeholder.com/400",
-    },
-  ];
+      const data =  await response.json();
+      console.log('data', data);
+      setProducts(data);
+    }
+
+    fetchProducts();
+  }, [])
+
+  // // Todo: replace with api call fetch 
+  // const products = [
+  //   {
+  //     title: "Example Product 1",
+  //     price: 29.99,
+  //     description: "This is a detailed description of the product.",
+  //     category: "Electronics",
+  //     image: "https://via.placeholder.com/400",
+  //   },
+  //   {
+  //     title: "Example Product 2",
+  //     price: 49.99,
+  //     description: "Another detailed description of a different product.",
+  //     category: "Home Appliances",
+  //     image: "https://via.placeholder.com/400",
+  //   },
+  // ];
 
   const handleViewDetails = (product) => {
     setSelectedProduct(product);
@@ -37,7 +54,7 @@ function ProductPage() {
   };
 
   return (
-    <Container>
+    <Container className="mt-5">
       <Row>
         {products.map((product, index) => (
           <Col key={index} md={4} className="mb-4">
