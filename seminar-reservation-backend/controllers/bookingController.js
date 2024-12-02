9915b02e89ca7188690de2c8780c6506f26a775d
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-02 21:27:47 +0800

    implement email notification

--- seminar-reservation-backend/controllers/bookingController.js
+++ seminar-reservation-backend/controllers/bookingController.js
@@ -1,5 +1,6 @@
 import Booking from '../models/Booking.js';
 import Seminar from '../models/Seminar.js';
+import sendEmail from '../utils/email.js';
 
 const createBooking = async (req, res) => {
   try {
@@ -12,6 +13,46 @@ const createBooking = async (req, res) => {
     const booking = await Booking.create({ user: req.user.id, seminar: seminarId });
     seminar.slotsAvailable -= 1;
     await seminar.save();
+
+    const { firstName: attendeeName } = req.user;
+    const { title, date: seminarDate, timeFrame, venue  } = seminar;
+
+    const htmlContent = `
+        <html>
+        <body>
+            <h1>Seminar Registration Confirmation</h1>
+            <p>Dear ${attendeeName},</p>
+            <p>Thanks for registering for <strong>${seminar.title}</strong>!</p>
+            <p><strong>Event Details:</strong></p>
+            <ul>
+            <li>Date: ${seminarDate}</li>
+            <li>Time: ${timeFrame.from} to ${timeFrame.to} </li>
+            <li>Location: ${venue}</li>
+            </ul>
+            <p>If you have any questions, contact us at seminar@example.com.</p>
+            <p>We look forward to seeing you there!</p>
+        </body>
+        </html>
+    `;
+
+    const plainTextContent = `
+    Seminar Registration Confirmation
+
+    Dear ${attendeeName},
+
+    Thank you for registering for ${title}!
+
+    Event Details:
+    - Date: ${seminarDate}
+    - Time:  ${timeFrame.from} to ${timeFrame.to} 
+    - Location: ${venue}
+
+    If you have any questions, contact us at seminar@example.com.
+
+    We look forward to seeing you there!
+  `;
+
+    await sendEmail(req.user.email, 'Seminar Confirmation', plainTextContent, htmlContent)
 
     res.status(201).json({ message: 'Booking created successfully', booking });
   } catch (error) {
