328aff88bbbc02ff8c67d03bd2aac4bd59b4689e
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-02 19:29:16 +0800

    implement category dropdown

--- D18/activity/src/pages/Products.jsx
+++ D18/activity/src/pages/Products.jsx
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Spinner, Alert } from 'react-bootstrap';
 import ProductCard from '../components/ProductCard';
 import ProductDetailsModal from '../components/ProductDetailsModal';
+import CategoriesDropdown from '../components/CategoriesDropdown'; // Import the dropdown component
 
 function ProductPage() {
   const [selectedProduct, setSelectedProduct] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch('https://fakestoreapi.com/products');
-      if (!response.ok) {
-        setError('Error fetching products');
+      
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(`https://fakestoreapi.com/products${selectedCategory  === 'all' ? ''  : `/category/${selectedCategory}`}`);
+        if (!response.ok) {
+          throw new Error('Error fetching products');
+        }
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
-      const data = await response.json();
-      setProducts(data);
     };
 
     fetchProducts();
-  }, []);
+  }, [selectedCategory]);
 
   const handleViewDetails = (product) => {
     setSelectedProduct(product);
@@ -35,6 +48,23 @@ function ProductPage() {
 
   return (
     <Container className="mt-5">
+      <Row className="mb-4">
+        <Col xs={12}>
+          <CategoriesDropdown
+            onChange={setSelectedCategory}
+            value={selectedCategory}
+          />
+        </Col>
+      </Row>
+
+      {loading && (
+        <div className="d-flex justify-content-center">
+          <Spinner animation="border" variant="primary" />
+        </div>
+      )}
+
+      {error && <Alert variant="danger">{error}</Alert>}
+
       <Row>
         {products.map((product, index) => (
           <Col key={index} xs={12} sm={6} md={4} lg={3} className="mb-4">
