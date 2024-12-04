ab1a04e7f729c6aa30d46c88a5b192bd6eb69bce
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 14:53:38 +0800

    implement layouts

--- seminar-reservation-frontend/src/App.jsx
+++ seminar-reservation-frontend/src/App.jsx
@@ -1,29 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PublicLayout from './layouts/PublicLayout';
 import AuthenticatedLayout from './layouts/AuthenticatedLayout';
 import SeminarsPage from './pages/Seminars';
 import AddSeminarPage from './pages/AddSeminar';
 import Seminar from './pages/Seminar';
 import EditSeminarPage from './pages/EditSeminar';
 import Login from './pages/Login';
-
+import { AuthProvider } from './context/AuthContext';
+import DashboardPage from './pages/Dashboard';
 
 
 function App() {
-
   return (
-    <Router>
+    <AuthProvider>
+      <Router>
         <Routes>
-          <Route path='/login' element={<Login />} />
+          <Route element={<PublicLayout />}>
+              <Route path='/login' element={<Login />} />
+          </Route>
           <Route element={<AuthenticatedLayout />}>
-            <Route path='/' element={null} />
+            <Route path='/' element={<DashboardPage /> } />
             <Route path='/admin/seminars' element={<SeminarsPage />} />
             <Route path="/admin/seminars/add" element={<AddSeminarPage />} />
             <Route path="/admin/seminars/:id" element={<Seminar />} />
             <Route path="/admin/seminars/:id/edit" element={<EditSeminarPage />} />
           </Route>
         </Routes>
-    </Router>
-  )
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
