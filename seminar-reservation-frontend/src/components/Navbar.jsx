132f99b4d217da119b1d495cb6ee57ebe5738043
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 13:10:42 +0800

    home button handler

--- seminar-reservation-frontend/src/components/Navbar.jsx
+++ seminar-reservation-frontend/src/components/Navbar.jsx
@@ -10,6 +10,10 @@ const Navbar = () => {
     navigate('/login');
   };
 
+  const handleHomeClick = () => {
+    navigate('/admin/seminars');
+  }
+
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -17,12 +21,12 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Admin Seminar Dashboard</a>
+        <a className="btn btn-ghost text-xl" onClick={handleHomeClick}>Admin Seminar Dashboard</a>
       </div>
       <div className="flex-none gap-2">
-        <div className="form-control">
+        {/* <div className="form-control">
           <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-        </div>
+        </div> */}
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar" onClick={toggleMenu}>
             <div className="w-10 rounded-full">
