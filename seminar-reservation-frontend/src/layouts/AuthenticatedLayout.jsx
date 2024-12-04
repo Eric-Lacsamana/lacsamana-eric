91c73995ca76a4dfc8eb183697e32258655baffe
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 14:54:53 +0800

    enhance authenticated layout

--- seminar-reservation-frontend/src/layouts/AuthenticatedLayout.jsx
+++ seminar-reservation-frontend/src/layouts/AuthenticatedLayout.jsx
@@ -1,33 +1,33 @@
-// src/components/AuthenticatedLayout.js
 import React, { useContext, useEffect, useState } from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import { AuthContext } from '../context/AuthContext';
+import FloatingButton from '../components/FloatingButton';
 
 const AuthenticatedLayout = () => {
+  const navigate = useNavigate();
   const { isAuthenticated } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
- 
     if (isAuthenticated !== undefined) {
       setIsLoading(false);
     }
   }, [isAuthenticated]);
 
- 
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   if (!isAuthenticated) {
-    return <Navigate to="/login" />;
+    return navigate('/login');
   }
 
   return (
     <div className="min-h-screen">
       <Navbar />
-      <main className="container mx-auto p-6">
+      <main className="container mx-auto p-6 mt-16">
+        <FloatingButton />
         <Outlet />
       </main>
     </div>
