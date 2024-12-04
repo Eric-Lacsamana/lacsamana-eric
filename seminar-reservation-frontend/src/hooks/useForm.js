3749409f82b835d29902b72a81b7bddf272eb0ac
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 11:47:58 +0800

    handle nested values

--- seminar-reservation-frontend/src/hooks/useForm.js
+++ seminar-reservation-frontend/src/hooks/useForm.js
@@ -1,16 +1,33 @@
 import { useState } from 'react';
 
+
+function setNestedValue(obj, path, value) {
+  const keys = path.split('.');
+  let current = obj;
+
+  keys.forEach((key, index) => {
+    if (index === keys.length - 1) {
+      current[key] = value;
+    } else {
+      if (!current[key]) {
+        current[key] = {}; 
+      }
+      current = current[key];
+    }
+  });
+
+  return obj;
+}
+
 export function useForm(initialValues) {
   const [formData, setFormData] = useState(initialValues);
   const [error, setError] = useState(null);
   const [isLoading, setLoading] = useState(false);
-
+  console.log('FORM DATA', formData);
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+
+    setFormData((prevData) => setNestedValue({ ...prevData }, name, value));
   };
 
   const handleSubmit = (e, onSubmit) => {
