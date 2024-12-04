1b231a054176dfda6b4cdac32b94446c3a1daf8f
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 09:47:39 +0800

    login page

--- seminar-reservation-frontend/src/pages/Login.jsx
+++ seminar-reservation-frontend/src/pages/Login.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import LoginForm from '../components/LoginForm';
 
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (data) => {
+    const { email, password} = data;
 
     setLoading(true);
     setError(null);
@@ -46,41 +45,18 @@ const Login = () => {
       
       {error && <p className="text-red-500 mb-4">{error}</p>}
 
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-lg font-semibold mb-2">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input input-bordered w-full px-4 py-2 rounded-lg"
-            required
-          />
-        </div>
+      <LoginForm onSubmit={handleSubmit} />
 
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-lg font-semibold mb-2">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="input input-bordered w-full px-4 py-2 rounded-lg"
-            required
-          />
-        </div>
-
-        <div className="mb-4 text-center">
-          <button
-            type="submit"
-            className="btn btn-primary w-full py-2 px-4 rounded-lg text-white"
-            disabled={loading}
-          >
-            {loading ? 'Logging in...' : 'Login'}
-          </button>
-        </div>
-      </form>
+      <div className="mb-4 text-center">
+        <button
+          form="login-form"
+          type="submit"
+          className="btn btn-primary w-full py-2 px-4 rounded-lg text-white"
+          disabled={loading}
+        >
+          {loading ? 'Logging in...' : 'Login'}
+        </button>
+      </div>
     </div>
   );
 };
