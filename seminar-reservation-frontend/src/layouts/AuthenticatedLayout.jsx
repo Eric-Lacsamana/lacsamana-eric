cb72fe0fa4dbf7e611315ca43b7a330bb6c2a012
Eric Lacsamana <eric.lacsamana07@gmail.com>
2024-12-04 13:31:39 +0800

    apply auth context

--- seminar-reservation-frontend/src/layouts/AuthenticatedLayout.jsx
+++ seminar-reservation-frontend/src/layouts/AuthenticatedLayout.jsx
@@ -1,19 +1,32 @@
-// AuthenticatedLayout.js
-import React from 'react';
+// src/components/AuthenticatedLayout.js
+import React, { useContext, useEffect, useState } from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import { AuthContext } from '../context/AuthContext';
 
 const AuthenticatedLayout = () => {
-  const token = localStorage.getItem('jwtToken');
+  const { isAuthenticated } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(true);
 
-  if (!token) {
+  useEffect(() => {
+ 
+    if (isAuthenticated !== undefined) {
+      setIsLoading(false);
+    }
+  }, [isAuthenticated]);
+
+ 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
 
   return (
     <div className="min-h-screen">
       <Navbar />
-
       <main className="container mx-auto p-6">
         <Outlet />
       </main>
